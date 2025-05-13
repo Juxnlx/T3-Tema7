860e77f6ac467684bc6531b8a6ed359cc7b6360c
juanl2004 <juanluis.barrionuevo@iesnervion.es>
2025-05-13 20:35:30 +0200

    Ejercicio 3 - Completo

--- Tema7/src/boletin1/ejercicio3/Ejercicio3.java
+++ Tema7/src/boletin1/ejercicio3/Ejercicio3.java
@@ -1,29 +1,24 @@
-package ejercicio3;
+package boletin1.ejercicio3;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import utiles.Constantes;
+
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// Creamos la variable url como String para almacenar la ruta donde se encuentra
-		// la base de datos.
-		String url = "jdbc:mysql://localhost/InstitutoDB";
 
-		// Creamos la variable usuario como String para almacenar el nombre de usuario
-		String usuario = "root";
+		// Creamos la conexión con el método DriverManager con la base de datos
+		// InstitutoDB. Hacemos uso del método getConnection y les pasamos como
+		// parametro las constantes necesarias para crear la conexión.
+		try (Connection con = DriverManager.getConnection(Constantes.URL, Constantes.USUARIO, Constantes.PASSWORD)) {
 
-		// Creamos la variable password como String para almacenar la contraseña del
-		// usuario.
-		String password = "Juanl2004";
-
-		try (Connection con = DriverManager.getConnection(url, usuario, password)) {
-
-			// Creamos el objeto de tipo Statement.
+			// Creamos el objeto de tipo Statement para ejecutar la sentencia.
 			Statement st = con.createStatement();
-			
+
 			// Creamos la variable eliminarEstudiante como String para almacenar la
 			// sentencia que ejecutaremos para eliminar un estudiante.
 			String eliminarEstudiante = "DELETE FROM estudiantes WHERE id_estudiante = 4";
@@ -40,10 +35,12 @@ public class Ejercicio3 {
 			// que se han eliminado.
 			int filasProfesor = st.executeUpdate(eliminarProfesor);
 
-			//Imprimimos la cantidad de alumnos y profesores que se han eliminado
+			// Imprimimos la cantidad de alumnos y profesores que se han eliminado
 			System.out.println("Alumnos eliminados: " + filasEstudiante);
 			System.out.println("Profesores eliminados: " + filasProfesor);
 
+			// Capturamos esta excepción para mostrar un error en caso de que no se pueda
+			// establecer conexión con la base de datos.
 		} catch (SQLException e) {
 			System.out.println("Error con la base de datos: " + e);
 		}
