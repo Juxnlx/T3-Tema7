8f4390e9c446cd47b8aae2b66d7e422b24f83cd5
jlbarrionuevo <jlbarrionuevo@106-02.info2>
2025-05-13 14:24:18 +0200

    Ejercicio 9 - Completo

--- Tema7/src/boletin1/ejercicio9/Ejercicio9.java
+++ Tema7/src/boletin1/ejercicio9/Ejercicio9.java
@@ -6,23 +6,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import utiles.Constantes;
+
 public class Ejercicio9 {
 
 	public static void main(String[] args) {
-		// Creamos la variable url como String para almacenar la ruta donde se encuentra
-		// la base de datos.
-		String url = "jdbc:mysql://localhost/InstitutoDB";
 
-		// Creamos la variable usuario como String para almacenar el nombre de usuario
-		String usuario = "root";
+		// Creamos la conexión con el método DriverManager con la base de datos
+		// InstitutoDB. Hacemos uso del método getConnection y les pasamos como
+		// parametro las constantes necesarias para crear la conexión.
+		try (Connection con = DriverManager.getConnection(Constantes.URL, Constantes.USUARIO, Constantes.PASSWORD)) {
 
-		// Creamos la variable password como String para almacenar la contraseña del
-		// usuario.
-		String password = "Juanl2004";
-
-		try (Connection con = DriverManager.getConnection(url, usuario, password)) {
-
-			// Creamos el objeto de tipo Statement
+			// Creamos el objeto de tipo Statement para ejecutar la sentencia.
 			Statement st = con.createStatement();
 
 			// Creamos la variable consulta como String para almacenar la instrucción de la
@@ -57,6 +52,8 @@ public class Ejercicio9 {
 				System.out.println("Nombre: " + nombre + " | Fecha de nacimiento: " + fechaNacimiento);
 			}
 
+			// Capturamos esta excepción para mostrar un error en caso de que no se pueda
+			// establecer conexión con la base de datos.
 		} catch (SQLException e) {
 			System.out.println("Error con la base de datos: " + e);
 		}
