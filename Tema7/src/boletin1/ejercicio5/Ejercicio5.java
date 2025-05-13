855a53af40212ff22b3fc2a14b36591a2349d850
juanl2004 <juanluis.barrionuevo@iesnervion.es>
2025-05-13 20:17:39 +0200

    Ejercicio 5 - Completo

--- Tema7/src/boletin1/ejercicio5/Ejercicio5.java
+++ Tema7/src/boletin1/ejercicio5/Ejercicio5.java
@@ -1,6 +1,7 @@
-package ejercicio5;
+package boletin1.ejercicio5;
 
 import java.sql.Statement;
+import utiles.Constantes;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,22 +10,12 @@ public class Ejercicio5 {
 
 	public static void main(String[] args) {
 
-		// Creamos la variable url como String para almacenar la ruta donde se encuentra
-		// la base de datos.
-		String url = "jdbc:mysql://localhost/InstitutoDB";
+		// Creamos la conexión con el método DriverManager con la base de datos
+		// InstitutoDB. Hacemos uso del método getConnection y les pasamos como
+		// parametro las constantes necesarias para crear la conexión.
+		try (Connection con = DriverManager.getConnection(Constantes.URL, Constantes.USUARIO, Constantes.PASSWORD)) {
 
-		// Creamos la variable usuario como String para almacenar el nombre de usuario
-		String usuario = "root";
-
-		// Creamos la variable password como String para almacenar la contraseña del
-		// usuario.
-		String password = "Juanl2004";
-
-		// Creamos una conexión con la base de datos usando el método estático
-		// DriverManager.
-		try (Connection con = DriverManager.getConnection(url, usuario, password)) {
-
-			// Creamos el objeto de tipo Statement
+			// Creamos el objeto de tipo Statement para ejecutar la sentencia.
 			Statement st = con.createStatement();
 
 			// Disponemos del método executeUpdate para hacer la intrucción que se le pase
@@ -42,8 +33,12 @@ public class Ejercicio5 {
 					"INSERT INTO Calificaciones (id_estudiante, id_curso, id_profesor, tipo_evaluacion, nota, fecha_evaluacion) "
 							+ "VALUES (3, 2, 2, 'Examen', 9.75, '2025-04-05')");
 
+			// Mostramos un mensaje de que las calificaciones han sido añadido
+			// correctamente.
 			System.out.println("Nuevas calificaciones añadidas correctamente.");
 
+			// Capturamos esta excepción para mostrar un error en caso de que no se pueda
+			// establecer conexión con la base de datos.
 		} catch (SQLException e) {
 			System.out.println("Error con la base de datos: " + e);
 		}
