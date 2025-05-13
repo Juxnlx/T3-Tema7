487b2a6fd0d4163d1e5e877e367b552ce4247c82
jlbarrionuevo <jlbarrionuevo@106-02.info2>
2025-05-13 13:27:37 +0200

    Constantes

---
+++ Tema7/src/utiles/Constantes.java
@@ -0,0 +1,15 @@
+package utiles;
+
+public class Constantes {
+	
+	// Creamos la constante URL como String para almacenar la ruta donde se encuentra
+	// la base de datos.
+	public static final String URL = "jdbc:mysql://localhost/InstitutoDB";
+
+	// Creamos la constante USUARIO como String para almacenar el nombre de usuario
+	public static final String USUARIO = "root";
+
+	// Creamos la constante PASSWORD como String para almacenar la contraseña del
+	// usuario.
+	public static final String PASSWORD = "Juanl2004";
+}
