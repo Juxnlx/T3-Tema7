c375643eed2c0e34840e019975d0065be613b949
jlbarrionuevo <jlbarrionuevo@106-02.info2>
2025-05-14 08:57:08 +0200

    Ejercicio 4 - Completo

--- Tema7/src/boletin1/ejercicio4/Ejercicio4.java
+++ Tema7/src/boletin1/ejercicio4/Ejercicio4.java
@@ -1,11 +1,10 @@
 package boletin1.ejercicio4;
 
-import java.sql.Statement;
-
 import utiles.Constantes;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Ejercicio4 {
@@ -17,30 +16,84 @@ public class Ejercicio4 {
 		// parametro las constantes necesarias para crear la conexión.
 		try (Connection con = DriverManager.getConnection(Constantes.URL, Constantes.USUARIO, Constantes.PASSWORD)) {
 
-			// Creamos el objeto de tipo Statement para ejecutar la sentencia.
-			Statement st = con.createStatement();
+			// Creamos el array res como un array unidimensional para almacenar el número de
+			// filas modificadas.
+			int res[];
 
-			// Disponemos del método executeUpdate para hacer la intrucción que se le pase
-			// por parametro. En este caso hacemos varios insert de nuevas asignaturas en la
-			// tabla cursos.
-			st.executeUpdate("INSERT INTO cursos (nombre, descripcion, año_escolar) "
-					+ "VALUES ('Historia 1º', 'Historia para primer año', 2025)");
+			// Creamos la variable insert como String para almacenar la instrucción insert
+			// sql.
+			String insert = "INSERT INTO cursos (nombre, descripcion, año_escolar) VALUES (?, ?, ?)";
 
-			st.executeUpdate("INSERT INTO cursos (nombre, descripcion, año_escolar) "
-					+ "VALUES ('Biología 1º', 'Biología para primer año', 2025)");
+			// Creamos el objeto de tipo PreparedStatement para sustituir valores '?'.
+			PreparedStatement ps = con.prepareStatement(insert);
 
-			st.executeUpdate("INSERT INTO cursos (nombre, descripcion, año_escolar) "
-					+ "VALUES ('Educación Física 1º', 'Educación Física para primer año', 2025)");
+			// Establecemos el primer parámetro ? con el nuevo nombre de la asignatura
+			// Historia 1º.
+			ps.setString(1, "Historia 1º");
+			// Establecemos el segundo parámetro ? con la descripción de la asignatura
+			// Historia 1º.
+			ps.setString(2, "Historia para primer año");
+			// Establecemos el tercer parámetro ? con el año escolar de la asignatura
+			// Historia 1º.
+			ps.setInt(3, 2025);
+			// Vamos añadiendo la asignatura para luego ser ejecutada.
+			ps.addBatch();
 
-			st.executeUpdate("INSERT INTO cursos (nombre, descripcion, año_escolar) "
-					+ "VALUES ('Música 1º', 'Música para primer año', 2025)");
+			// Establecemos el primer parámetro ? con el nuevo nombre de la asignatura
+			// Biología 1º.
+			ps.setString(1, "Biología 1º");
+			// Establecemos el segundo parámetro ? con la descripción de la asignatura
+			// Biología 1º.
+			ps.setString(2, "Biología para primer año");
+			// Establecemos el tercer parámetro ? con el año escolar de la asignatura
+			// Biología 1º.
+			ps.setInt(3, 2025);
+			// Vamos añadiendo la asignatura para luego ser ejecutada.
+			ps.addBatch();
 
-			st.executeUpdate("INSERT INTO cursos (nombre, descripcion, año_escolar) "
-					+ "VALUES ('Tecnología 1º', 'Tecnología para primer año', 2025)");
+			// Establecemos el primer parámetro ? con el nuevo nombre de la asignatura
+			// Educación Física 1º.
+			ps.setString(1, "Educación Física 1º");
+			// Establecemos el segundo parámetro ? con la descripción de la asignatura
+			// Educación Física 1º.
+			ps.setString(2, "Educación Física para primer año");
+			// Establecemos el tercer parámetro ? con el año escolar de la asignatura
+			// Educación Física 1º.
+			ps.setInt(3, 2025);
+			// Vamos añadiendo la asignatura para luego ser ejecutada.
+			ps.addBatch();
+
+			// Establecemos el primer parámetro ? con el nuevo nombre de la asignatura
+			// Música 1º.
+			ps.setString(1, "Música 1º");
+			// Establecemos el segundo parámetro ? con la descripción de la asignatura
+			// Música 1º.
+			ps.setString(2, "Música para primer año");
+			// Establecemos el tercer parámetro ? con el año escolar de la asignatura
+			// Música 1º.
+			ps.setInt(3, 2025);
+			// Vamos añadiendo la asignatura para luego ser ejecutada.
+			ps.addBatch();
+
+			// Establecemos el primer parámetro ? con el nuevo nombre de la asignatura
+			// Tecnología 1º.
+			ps.setString(1, "Tecnología 1º");
+			// Establecemos el segundo parámetro ? con la descripción de la asignatura
+			// Tecnología 1º.
+			ps.setString(2, "Tecnología para primer año");
+			// Establecemos el tercer parámetro ? con el año escolar de la asignatura
+			// Tecnología 1º.
+			ps.setInt(3, 2025);
+			// Vamos añadiendo la asignatura para luego ser ejecutada.
+			ps.addBatch();
+
+			// Ejecutamos todas las instrucciones que hemos ido almacenando. Almacenamos en
+			// nuestro array res el número de filas que se han modificado.
+			res = ps.executeBatch();
 
 			// Imprimimos un mensaje mostrando que las inserciones se han añadido
 			// correctamente.
-			System.out.println("Inserciones realizadas correctamente.");
+			System.out.println("Inserciones realizadas correctamente. Número de filas: " + res.length);
 
 			// Capturamos esta excepción para mostrar un error en caso de que no se pueda
 			// establecer conexión con la base de datos.
